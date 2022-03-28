c665bbc0f50b19924f05e6a986dd42dd894fe68e
Philipp Otto <philotto95@gmail.com>
2022-03-28 14:47:34 +0200

    Check for correct code input

--- rb/user_input.rb
+++ rb/user_input.rb
@@ -3,8 +3,32 @@
 # module UserInput
 module UserInput
   def get_guess
-    guess = ask_for_guess()
-    transform_guess_into_integer_array(guess)
+    correct_guess = false
+    guess_array = []
+    until correct_guess
+      guess = ask_for_guess()
+      correct_guess = four_digit_code?(guess)
+      guess_array = transform_guess_into_integer_array(guess)
+      correct_guess = code_in_range?(guess_array)
+    end
+    guess_array
+  end
+
+  def four_digit_code?(guess)
+    return true unless guess.to_i.zero? || guess.length != 4
+
+    puts 'Please enter a four digit code.'
+    false
+  end
+
+  def code_in_range?(guess_array)
+    guess_array.each do |digit|
+      next if digit.between?(1, 6)
+
+      puts 'Please enter a code with digits between 1 and 6.'
+      return false
+    end
+    true
   end
 
   def ask_for_guess
